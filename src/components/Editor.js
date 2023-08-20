23282f664cea1b763d4ffd1c85eea7f10fb832be
Suraj <42367885+suraj0041@users.noreply.github.com>
2023-08-20 20:22:07 +0530

    Verify token Add verify token feature to editor

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,6 +1,39 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import useAuth from "../hooks/useAuth";
 
 const Editor = () => {
+  const { auth, setAuth } = useAuth();
+  const [verified, setVerified] = useState(false);
+
+  const onClickHandler = () => {
+    if (auth?.accessToken) {
+      // console.log(
+      //   JSON.stringify({
+      //     username: auth?.user,
+      //     password: auth?.pwd,
+      //     accessToken: auth?.accessToken
+      //   })
+      // );
+      fetch("https://d7s2x6-8080.csb.app/verify", {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, *cors, same-origin
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${auth?.accessToken}`
+        }
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data[0]?.username === auth?.user) {
+            setVerified(true);
+          } else {
+            setAuth({});
+          }
+        })
+        .catch((err) => console.log(err));
+    }
+  };
   return (
     <section>
       <h1>Editors Page</h1>
@@ -9,6 +42,10 @@ const Editor = () => {
       <div className="flexGrow">
         <Link to="/">Home</Link>
       </div>
+      <div className="flexGrow">
+        <button onClick={onClickHandler}>Verify token</button>
+      </div>
+      <div className="flexGrow">{verified ? "verified" : "Verify"}</div>
     </section>
   );
 };
